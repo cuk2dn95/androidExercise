a08ab915dedd741ec9e8337862dd5d5a2f386a7b
Quoc Khanh <cuk2dn96@gmail.com>
2017-09-08 15:12:49 +0700

    facebook interface exercise

---
+++ app/src/main/java/com/example/pc/myapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.pc.myapplication;
+
+import android.graphics.Color;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity  {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+
+
+}
